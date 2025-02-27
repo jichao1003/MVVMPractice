e0262f3b0e21cf111cede7e3690377125c161788
jichao <jichao@eswincomputing.com>
2025-02-27 15:49:47 +0800

    [MVVMDemo]
    [Describe] the problem/rootcouse/feature/improvement
    [Solution]
    [ImpactDomain]
    [Testing]
    
    Issue:

---
+++ MVVMDemo2/LoginViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+
+namespace MVVMDemo2
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private LoginModel _loginModel;
+        private Login _loginView;
+
+        public LoginViewModel(Login view)
+        {
+            _loginModel = new LoginModel();
+            _loginView = view;
+        }
+
+        // 绑定到登录界面文本框的属性，用于获取和设置用户名和密码 
+        public string UserName
+        {
+            get { return _loginModel.UserName; }
+            set
+            {
+                _loginModel.UserName = value;
+                OnPropertyChanged(UserName);
+            }
+        }
+
+        public string Password
+        {
+            get { return _loginModel.Password; }
+            set
+            {
+                _loginModel.Password = value;
+                OnPropertyChanged(Password);
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)	// 触发属性更改通知的方法
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void LoginFunc()	// 处理登录操作
+        {
+            if (UserName == "admin" && Password == "123456")
+            {
+                MainWindow main = new MainWindow();
+                main.Show();
+                _loginView.Close();
+            }
+            else
+            {
+                MessageBox.Show("用户名或密码错误");
+                UserName = "";
+                Password = "";
+            }
+        }
+
+        private bool CanLoginExecute()
+        {
+            return true;
+        }
+
+        public ICommand LoginAction	// 绑定到登录按钮的命令属性
+        {
+            get
+            {
+                return new RelayCommand(LoginFunc, CanLoginExecute);	//（执行体，判断条件）
+            }
+        }
+    }
+}
---
+++ MVVMDemo2/RelayCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMDemo2
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;   // 需要执行的操作（命令体）
+        private readonly Func<bool> _canExecute;    // 命令是否可以执行的逻辑
+
+        public RelayCommand(Action action, Func<bool> canExecute)
+        {
+            _execute = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)    // ICommand接口方法之一，用于判断命令是否可以执行
+        {
+            if (_canExecute == null)
+            {
+                return true;	// 命令始终可以执行
+            }
+            else
+            {
+                return _canExecute();	// 调用 _canExecute() 获取判断结果
+            }
+        }
+
+        public void Execute(object parameter)   // ICommand接口方法之一 用于执行命令体，调用 _execute 所存储的操作
+        {
+            _execute?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged	// ICommad接口中的事件，当命令的可执行状态发生变化时，触发此事件来通知界面元素更新
+        {
+            add
+            {
+                if (_canExecute != null) { CommandManager.RequerySuggested += value; }
+            }
+            remove
+            {
+                if (_canExecute != null) { CommandManager.RequerySuggested -= value; }
+            }
+        }
+    }
+}
